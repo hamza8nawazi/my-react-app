cf8c2e4d6f0a638fa1667e6776cd88a18b960f40
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-10-24 21:13:27 +0500

    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Home from './pages/home';
 import Button from './components/button';
 
+
+
 function App() {
   return (
     <div>
       <Home />
-      <Button />
+      <Button label="Click Me" />
+      
     </div>
   );
 }
---
+++ src/layout/navbar/index.js
@@ -0,0 +1,36 @@
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+      <div className="navbar-left">
+        <a href="#home">Dropbox</a>
+      </div>
+      <div className="navbar-center">
+        <ul>
+          <li>
+            <a href="#why-dropbox">Why Dropbox?</a>
+          </li>
+          <li>
+            <a href="#products">Products</a>
+          </li>
+          <li>
+            <a href="#solutions">Solutions</a>
+          </li>
+          <li>
+            <a href="#pricing">Pricing</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <a href="#contact">Contact</a>
+        <a href="#signup">Sign up</a>
+        <a href="#signin">Sign in</a>
+        <a href="#get-started" className="btn-get-started">
+          Get started
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
