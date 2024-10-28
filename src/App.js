707a23f309c11b58b574db3a8edd7339be2be75a
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-10-28 16:16:45 +0500

    added list component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import { useState } from "react";
+// import { useState } from "react";
 import './App.css';
 import Home from './pages/home';
-import Button from './components/button';
-import List from './components/lists';
+// import Button from './components/button';
+// import List from './components/lists';
 
 function App() {
-  const [isActiveFirstItem , setIsActiveFirstItem] = useState(true);
-  const [isActiveSecondItem , setIsActiveSecondItem ] = useState(false);
-  const [isActiveThirdItem , setIsActiveThirdItem ] = useState(true);
-  const [isActiveFourthItem , setIsActiveFourthItem ] = useState(false);
+  // const [isActiveFirstItem , setIsActiveFirstItem] = useState(true);
+  // const [isActiveSecondItem , setIsActiveSecondItem ] = useState(false);
+  // const [isActiveThirdItem , setIsActiveThirdItem ] = useState(true);
+  // const [isActiveFourthItem , setIsActiveFourthItem ] = useState(false);
 
-  const myitems = [
-    { title: 'item 1', ischecked: isActiveFirstItem},
-    { title: 'item 2', ischecked: isActiveSecondItem},
-    { title: 'item 3', ischecked: isActiveThirdItem},
-    { title: 'item 4', ischecked: isActiveFourthItem }
-  ];
+  // const myitems = [
+  //   { title: 'item 1', ischecked: isActiveFirstItem},
+  //   { title: 'item 2', ischecked: isActiveSecondItem},
+  //   { title: 'item 3', ischecked: isActiveThirdItem},
+  //   { title: 'item 4', ischecked: isActiveFourthItem }
+  // ];
 
   
-  const reverse = () => {
-    setIsActiveFirstItem(prevA => !prevA); 
-    setIsActiveSecondItem(prevB => !prevB); 
-    setIsActiveThirdItem(prevC => !prevC); 
-    setIsActiveFourthItem(prevD => !prevD); 
-  }
+  // const reverse = () => {
+  //   setIsActiveFirstItem(!isActiveFirstItem); 
+  //   setIsActiveSecondItem(!isActiveSecondItem); 
+  //   setIsActiveThirdItem(!isActiveThirdItem); 
+  //   setIsActiveFourthItem(!isActiveFourthItem); 
+  // }
 
   return (
     <div>
       <Home />
-      <Button label="Click Me" onClick={reverse} /> 
-      <List items={myitems} />
+      {/* <Button label="Click Me" onClick={reverse} /> 
+      <List items={myitems} /> */}
     </div>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,33 @@
 import Navbar from "../../layout/navbar";
 import "./Home.css";
+import Button from "../../components/button";
+import List from "../../components/lists";
+import { useState } from "react";
 const Home = () => {
+  const [isActiveFirstItem, setIsActiveFirstItem] = useState(true);
+  const [isActiveSecondItem, setIsActiveSecondItem] = useState(false);
+  const [isActiveThirdItem, setIsActiveThirdItem] = useState(true);
+  const [isActiveFourthItem, setIsActiveFourthItem] = useState(false);
+
+  const myitems = [
+    { title: "item 1", ischecked: isActiveFirstItem },
+    { title: "item 2", ischecked: isActiveSecondItem },
+    { title: "item 3", ischecked: isActiveThirdItem },
+    { title: "item 4", ischecked: isActiveFourthItem },
+  ];
+
+  const reverse = () => {
+    setIsActiveFirstItem(!isActiveFirstItem);
+    setIsActiveSecondItem(!isActiveSecondItem);
+    setIsActiveThirdItem(!isActiveThirdItem);
+    setIsActiveFourthItem(!isActiveFourthItem);
+  };
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Button label="Click Me" onClick={reverse} />
+      <List items={myitems} />
+    </>
   );
 };
 
